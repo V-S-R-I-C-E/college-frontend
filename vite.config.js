fe116182c49d583aed89e15644c8f64c03e90d6f
V-S-R-I-C-E <vijayraj3951f@gmail.com>
2025-08-27 02:15:53 +0530

    DEBUG

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/college-frontend/', 
+  base: "/college-frontend/", // 👈 must match your GitHub repo name
+});
